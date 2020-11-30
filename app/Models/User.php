93243a73b137a6fd99ee88c4daeaa9cd60ca3203
nicolas sabogal <nicolassabogal85@gmail.com>
2020-11-30 06:56:26 -0500

    company table and relationaship

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'company_id'
     ];
 
     /**
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function company(){
+        return $this->belongsTo('App\Models\Company');
+    }
 }
